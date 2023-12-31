04470af205a19a7ea3f26d127114fe0a386272ff
Adefela Fakorode <adefakorode@gmail.com>
2023-12-31 00:08:10 -0500

    added hover cursor

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,14 +5,14 @@ const NavBar = () => {
   const [nav, setNav] = useState(false)
   const handleClick = () => setNav(!nav)
   return (
-    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-primary text-primaryTextColor font-bold">
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-primary  font-bold">
       <div className="hidden md:flex ">
         {/* Menu */}
         <ul className="md:flex hidden">
-          <li>Home</li>
-          <li>About</li>
-          <li>Project</li>
-          <li>Contact</li>
+          <li className="text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">Home</li>
+          <li className="text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">About</li>
+          <li className="text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">Project</li>
+          <li className="text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">Contact</li>
         </ul>
       </div>
 
@@ -23,7 +23,7 @@ const NavBar = () => {
 
       {/* Mobile Menu */}
       <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center'}>
-        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl ">Home</li>
         <li className="py-6 text-4xl">About</li>
         <li className="py-6 text-4xl">Project</li>
         <li className="py-6 text-4xl">Contact</li>

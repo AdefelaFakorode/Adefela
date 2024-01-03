e4d43a4d5161ba2b877a17fc08cdf5dc6ba9fe2f
Adefela Fakorode <adefakorode@gmail.com>
2024-01-02 23:53:23 -0500

    added dropshadow to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,16 @@
-import {useState} from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  const navbarStyle = {
+    boxShadow: "0px 4px 6px rgba(0, 0, 0, .3)"
+  };
+
   return (
-    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-primary  font-bold">
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-primary  font-bold" style={navbarStyle}>
       <div className="hidden sm:flex ">
         {/* Menu */}
         <ul className="sm:flex hidden">
@@ -22,7 +27,7 @@ const NavBar = () => {
       </div>
 
       {/* Mobile Menu */}
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center '}>
+      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-primary flex flex-col justify-center items-center '} style={navbarStyle}>
         <li className="py-6 text-4xl text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">Home</li>
         <li className="py-6 text-4xl text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">About</li>
         <li className="py-6 text-4xl text-primaryTextColor translate-colors duration-300 hover:text-bHoverColor">Project</li>

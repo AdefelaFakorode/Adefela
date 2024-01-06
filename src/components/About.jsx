11deb6b46e70652c864e0850af160184d3750094
Adefela Fakorode <adefakorode@gmail.com>
2024-01-06 00:41:27 -0600

    working on desktop resp of about pg

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,33 +4,38 @@ import BBP2 from "../assets/BBP2.jpg";
 
 const About = () => {
   return (
-    <div name="about" className="w-full h-screen bg-primary ">
-      {/*About text*/}
-      <div className="flex justify-center text-primaryTextColor font-bold text-3xl mb-3 mt-14">
-        <h1>About Me</h1>
-      </div>
+    <div
+      name="about"
+      className="xl:flex xl:justify-end xl:flex-row-reverse w-full h-screen  bg-primary ">
 
-      {/*Card*/}
-      <div className="flex justify-center">
-        <div className=" w-96 h-72 rounded-3xl  bg-secondary">
-          <p className="m-8 text-white w-80">
-            My name is Adefela Fakorode. Currently, I'm an undergraduate
-            student-athlete at the College of Staten Island, majoring in
-            Computer Science. Deeply immersed in the world of Software
-            Development, my passion lies in the exciting realms of full-stack
-            development, intuitive UI/UX design, the marvels of artificial
-            intelligence, and fascinating human-computer interactions.
-          </p>
+      <div>
+        {/*About text*/}
+        <div className="flex justify-center text-primaryTextColor font-bold sm:text-xl xl:text-6xl mb-3 mt-14">
+          <h1>About Me</h1>
+        </div>
+
+        {/*Card*/}
+        <div className="flex justify-center ">
+          <div className=" w-96 h-72 rounded-3xl  bg-secondary">
+            <p className="m-8 text-white w-80">
+              My name is Adefela Fakorode. Currently, I'm an undergraduate
+              student-athlete at the College of Staten Island, majoring in
+              Computer Science. Deeply immersed in the world of Software
+              Development, my passion lies in the exciting realms of full-stack
+              development, intuitive UI/UX design, the marvels of artificial
+              intelligence, and fascinating human-computer interactions.
+            </p>
+          </div>
         </div>
       </div>
 
       {/*Images*/}
-      <div className="mt-10 flex justify-center space-x-10">
+      <div className="xl:mx-24 xl:mt-28 mt-16 flex justify-center space-x-10">
         <div className="flex">
-        <img src={BBP1} alt="bbp1" className="w-36 h-52 rounded-3xl" />
+          <img src={BBP1} alt="bbp1" className="xl:w-64 xl:h-96 h-64 rounded-3xl" />
         </div>
         <div>
-        <img src={BBP2} alt="bbp2" className="w-36 h-52 rounded-3xl my-24" />
+          <img src={BBP2} alt="bbp2" className="xl:w-64 xl:h-96 w-44 h-62 rounded-3xl xl:my-32 my-24" />
         </div>
       </div>
     </div>
